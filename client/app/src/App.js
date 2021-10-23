aa403f54439ccb415ec1f34ea296aba41dbff556
Intiser <chowdhuryintiser19@gmail.com>
2021-10-23 20:25:36 +0600

    SignUp Page

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Form from "./Form";
+import "./Form.css";
+import FormLogin from "./FormLogin";
+import Form2 from "./Form2";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./NavComponents/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/register" component={Form} />
+          <Route path="/login" component={Form2} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+const Home = () => (
+  <div className="home-page">
+    <h1>This is home page</h1>
+  </div>
+);
 
 export default App;
